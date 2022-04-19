df7d53109756deb05590d7e94cb55d5f8aefb727
Jaime Alvarez <jealvarez123@gmail.com>
2022-04-19 16:57:05 -0500

    added the final bit of code

--- js/app.js
+++ js/app.js
@@ -14,5 +14,16 @@ const menu = {
     } else {
       console.log('No price')
     }
+  },
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today's meal is ${menu._meal} for $${menu._price}`;
+    } else {
+      return 'Whoops, something went wrong';
+    }
   }
 }
+menu.meal = 'Rack of lamb with baked ziti';
+menu.price = 17.25;
+
+console.log(menu.todaysSpecial);
